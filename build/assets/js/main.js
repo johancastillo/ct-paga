c2d4ad35880c5f321873c2d2eeb7452224be2c0b
Johan Castillo <jcjohan2707@gmail.com>
2021-01-25 14:47:03 -0400

    ToTop Component

--- build/assets/js/main.js
+++ build/assets/js/main.js
@@ -1,2 +1,17 @@
 "use strict";
-console.log('Hello World');
+//*****  To Up Component Start *****
+const toTop = document.getElementById('to-top');
+const hiddenToTop = () => {
+    if (scrollY === 0) {
+        let styles = toTop === null || toTop === void 0 ? void 0 : toTop.style;
+        styles.opacity = 0;
+    }
+    else if (scrollY > 100) {
+        let styles = toTop === null || toTop === void 0 ? void 0 : toTop.style;
+        styles.opacity = 1;
+    }
+};
+const action = () => window.scroll(0, 0);
+toTop === null || toTop === void 0 ? void 0 : toTop.addEventListener('click', action);
+addEventListener('scroll', hiddenToTop);
+//*****  To Up Component End *****
--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,1 +1,19 @@
-console.log('Hello World')
+//*****  To Up Component Start *****
+const toTop: HTMLElement | null = document.getElementById('to-top')
+
+const hiddenToTop = () => {
+    if(scrollY === 0){
+        let styles:any = toTop?.style
+        styles.opacity = 0
+    }else if(scrollY > 100){
+        let styles:any = toTop?.style
+        styles.opacity = 1
+    }
+}
+
+const action = () => window.scroll(0, 0)
+
+toTop?.addEventListener('click', action)
+
+addEventListener('scroll', hiddenToTop)
+//*****  To Up Component End *****
